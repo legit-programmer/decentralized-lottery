2f68153d61f7807a2d79fa944120ca26a6f97383
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-02 07:16:50 +0530

    setting up chainlink vrf

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFV2WrapperConsumerBase, Ownable {
     address[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface priceFeed;
@@ -15,10 +16,44 @@ contract Lottery is Ownable {
 
     LotteryState public state;
 
-    constructor(address _priceFeed) {
+    // Address LINK - hardcoded for Sepolia
+    address linkAddress = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+
+    // address WRAPPER - hardcoded for Sepolia
+    address wrapperAddress = 0xab18414CD93297B0d12ac29E63Ca20f515b3DB46;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 2;
+    uint32 numWords = 1;
+    uint256[] result;
+    uint256 lastRequestId;
+
+    constructor(address _priceFeed) VRFV2WrapperConsumerBase(linkAddress, wrapperAddress) {
         usdEntryFee=50 * (10**18);
         priceFeed = AggregatorV3Interface(_priceFeed);
         state = LotteryState.CLOSED;
+    }
+
+    function requestRandomWords()
+        external
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        requestId = requestRandomness(
+            callbackGasLimit,
+            requestConfirmations,
+            numWords
+        );
+        lastRequestId = requestId;
+        return requestId;
+    }
+
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        require(_requestId!=0, "request not yet fulfilled");
+        result = _randomWords;
+
+    }
+    function getRandomWords() external view returns(uint256[] memory randomWords){
+        return result;
     }
 
     function enter() public payable {
@@ -41,6 +76,8 @@ contract Lottery is Ownable {
 
     function endLottery() public onlyOwner {
         require(state==LotteryState.OPEN, "Lottery already closed...");
+        state = LotteryState.CALCULATE_WINNER;
+
 
     }
 }
--- tests/test_Lottery.py
+++ tests/test_Lottery.py
@@ -1,6 +1,6 @@
 #0.027 in eth minimum
 # 27000000000000000 in wei
-from brownie import accounts, Lottery, config, network
+from brownie import accounts, Lottery, config, network, VRFv2DirectFundingConsumer
 from web3 import Web3
 
 def test_minimumFee():
@@ -10,6 +10,12 @@ def test_minimumFee():
     print(fee)
     assert fee > Web3.toWei(0.025, 'ether')
     assert fee < Web3.toWei(0.029, 'ether')
+
+def test_randomWords():
+    contract = VRFv2DirectFundingConsumer.deploy({'from':accounts[0]})
+    requestid = contract.requestRandomWords({'from':accounts[0], 'gas_limit': 100000})
+    assert requestid!=0
+
 
 def main():
     test_minimumFee()
