c6ccdc87404a33950ae835655fba4989d9a1b305
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-03 23:13:48 +0530

    configuring priceFeed for non forked environment

--- tests/test_Lottery.py
+++ tests/test_Lottery.py
@@ -1,20 +1,28 @@
-#0.027 in eth minimum
+# 0.027 in eth minimum
 # 27000000000000000 in wei
-from brownie import accounts, Lottery, config, network, VRFv2DirectFundingConsumer
+from brownie import accounts, Lottery, config, network
 from web3 import Web3
+from dotenv import load_dotenv
+import os
+load_dotenv()
+contract = ''
+
 
 def test_minimumFee():
     print(network.show_active())
-    contract = Lottery.deploy(config['networks'][network.show_active()]['priceFeed'], {'from':accounts[0]})
-    fee= contract.getEntranceFee()
+    
+    requestid = 0
+    contract = Lottery.deploy(config['networks'][network.show_active()]['priceFeed'], {
+                            'from': accounts.add(os.getenv('PRIVATE_KEY'))})
+    fee = contract.getEntranceFee()
     print(fee)
+    requestid = contract.requestRandomWords({'from':accounts.add(os.getenv('PRIVATE_KEY')), 'gas_limit':500000})
     assert fee > Web3.toWei(0.025, 'ether')
     assert fee < Web3.toWei(0.029, 'ether')
-
-def test_randomWords():
-    contract = VRFv2DirectFundingConsumer.deploy({'from':accounts[0]})
-    requestid = contract.requestRandomWords({'from':accounts[0], 'gas_limit': 100000})
     assert requestid!=0
+    
+    
+
 
 
 def main():
