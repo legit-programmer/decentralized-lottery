628738ebce4f731e12b6d7f6b08c9126cf4654aa
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-05 21:21:35 +0530

    interacting with LINK contract on sepolia

--- tests/test_Lottery.py
+++ tests/test_Lottery.py
@@ -5,7 +5,7 @@ from web3 import Web3
 from dotenv import load_dotenv
 import os
 load_dotenv()
-contract = ''
+
 
 
 def test_minimumFee():
@@ -13,12 +13,12 @@ def test_minimumFee():
     
     requestid = 0
     contract = Lottery.deploy(config['networks'][network.show_active()]['priceFeed'], {
-                            'from': accounts.add(os.getenv('PRIVATE_KEY'))})
-    linkToken = Contract.from_explorer("0x6b175474e89094c44da98b954eedeac495271d0f")
-    linkToken.transfer(contract.address, 1e18, accounts.add(os.getenv('PRIVATE_KEY')))
+                            'from': accounts.add(os.getenv('ETH_PRIVATE_KEY')), 'gas_limit':5000000})
+    linkToken = Contract.from_explorer('0x779877A7B0D9E8603169DdbD7836e478b4624789')# LINK contract address at SEpolia
+    linkToken.transfer(contract.address, 1e18, {'from':accounts.add(os.getenv('ETH_PRIVATE_KEY'))})
     fee = contract.getEntranceFee()
     print(fee)
-    requestid = contract.requestRandomWords({'from':accounts.add(os.getenv('PRIVATE_KEY')), 'gas_limit':500000})
+    requestid = contract.requestRandomWords({'from':accounts.add(os.getenv('ETH_PRIVATE_KEY')), 'gas_limit':500000})
     assert fee > Web3.toWei(0.025, 'ether')
     assert fee < Web3.toWei(0.029, 'ether')
     assert requestid!=0
